2e512b674a3367c76ec3f928e6385dcc68f65b6c
Brenda Luana <brendalfcastro@gmail.com>
2026-04-21 18:06:52 -0300

    Lista 05 exercício 3

---
+++ lista05-ex03/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/card/Card'
+function App() {
+
+  return (
+    <>
+  <div className='grid-container'>
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+     <Card/>
+    </div>
+    </>
+    
+  )
+}
+
+export default App
---
+++ lista05-ex03/src/components/card/Card.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import '../../App.css'
+
+
+function Card() {
+
+  return (
+    <>
+      <div className='card'>
+        <h2 className='titulo'>Título da caixa</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum assumenda ad quod sed, fugit pariatur beatae velit perspiciatis asperiores facere quos hic deserunt, a exercitationem error voluptatem cum consequatur suscipit!</p>
+      </div>
+    </>
+  )
+}
+
+export default Card
